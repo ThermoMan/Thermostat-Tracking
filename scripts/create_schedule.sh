e46192a59cf184ed47cfc75d42005d1780f31b51
ThermoMan <thestalwart1-software@yahoo.com>
2012-07-11 02:15:46 -0500

    Still part of frist commit

---
+++ scripts/create_schedule.sh
@@ -0,0 +1,6 @@
+# Replace this with a shell script that creates unix cron jobs that do the same thing as the Windows Schedule lines below.
+
+# schtasks /create /tn ThermoUpdateStatus /tr "\"C:\xampp\htdocs\Thermo\scripts\thermo_update_status.bat\" " /st 00:00 /sc minute /mo 1 /ru WINDOWS_USER /rp PASSWORD
+#
+#schtasks /create /tn ThermoUpdateTemps /tr "\"C:\xampp\htdocs\Thermo\scripts\thermo_update_temps.bat\" " /st 00:00 /sc minute /mo 30 /ru WINDOWS_USER /rp PASSWORD
+#
