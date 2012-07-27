e37f62d2d0fdc8062353864188224a4661898f98
ThermoMan <thestalwart1-software@yahoo.com>
2012-07-27 02:47:29 -0500

    Show daily high temperature (on a second scale) as backdrop to run times

--- draw_runtimes.php
+++ draw_runtimes.php
@@ -25,53 +25,102 @@ if( !$link )
 }
 mysql_select_db( $db, $link ) or die( "cannot select DB" );            // Really should log this?
 
-$show_date = $_GET["show_date"];
+/* This chart no longer takes date as a parameter - but may take a date range in the future
+// Set default date to today
+$show_date = date( "Y-m-d" );
+if( isset( $_GET["show_date"] ) )
+{ // Use provided date
+  $show_date = $_GET["show_date"];
+}
 $date_pattern = "/[2]{1}[0]{1}[0-9]{2}-[0-9]{2}-[0-9]{2}/";
 if( !preg_match( $date_pattern, $show_date ) )
 {
   bobby_tables();
   return;
 }
+*/
 
-$sql = "SELECT date, heat_runtime, cool_runtime FROM " . $table_prefix . "run_times ORDER by date ASC;";
+// Find the limits of the run time data and match that to the outdoor temp data
+$sql = "SELECT min(date) AS min_date, max(date) AS max_date FROM " . $table_prefix . "run_times;";
+$result = mysql_query( $sql );
+$row = mysql_fetch_array( $result );
+$min_date = $row[ "min_date" ];
+$max_date = $row[ "max_date" ];
 
+//$sql = "SELECT date, heat_runtime, cool_runtime, MAX(outdoor_temp) AS outdoor_max FROM " . $table_prefix . "run_times ORDER by date ASC;";
+$sql = "SELECT date, heat_runtime, cool_runtime, "
+       . "(SELECT MAX(outdoor_temp) "
+       . " FROM " . $table_prefix . "temperatures "
+       . " WHERE " . $table_prefix . "run_times.date = DATE(" . $table_prefix . "temperatures.date) ) AS outdoor_max "
+       . "FROM " . $table_prefix . "run_times "
+       . "ORDER by date ASC;";
 $result = mysql_query( $sql );
 
 // Create and populate the pData object
 $MyData = new pData();
 
+// Set default boundaries for chart
+$chart_temp_min = $normal_low;
+$chart_temp_max = $normal_high;
+$chart_runtime_min = 0;
+$chart_runtime_max = 120;  // Set two hours as the default runtime scale max
+
+$first = true;
 while( $row = mysql_fetch_array( $result ) )
 {
-    $MyData->addPoints( $row['date'], "Labels" );
+  // There are too many dates to show them ALL on the x-axis - it turns into one black line.
+  if( $first )
+  { // Do show the WHOLE date for the first item.
+    $MyData->addPoints( $row["date"], "Labels" );
+    $first = false;
+  }
+  else
+  { // Thereafter show only MM-DD when you show anything at all
+    $MyData->addPoints( substr( $row['date'], 5, 5 ), "Labels" );
+  }
 
-  if( $row['heat_runtime'] != 'VOID' )
-  { // Assume that if one data point is bad, they both are.
-    $MyData->addPoints( $row['heat_runtime'], "Heat" );
-    $MyData->addPoints( $row['cool_runtime'], "Cool" );
+  if( $row["heat_runtime"] != "VOID" )
+  { // Assume that if one data point is bad, they all are.
+    $MyData->addPoints( $row["heat_runtime"], "Heat" );
+    if( $row["heat_runtime"] > $chart_runtime_max ) $chart_runtime_max = $row["heat_runtime"];
+
+    $MyData->addPoints( $row["cool_runtime"], "Cool" );
+    if( $row["cool_runtime"] > $chart_runtime_max ) $chart_runtime_max = $row["cool_runtime"];
+
+    $MyData->addPoints( $row["outdoor_max"], "Temperature" );
+    if( $row["outdoor_max"] < $chart_temp_min ) $chart_temp_min = $row["outdoor_max"];
+    if( $row["outdoor_max"] > $chart_temp_max ) $chart_temp_max = $row["outdoor_max"];
   }
   else
   {
     $MyData->addPoints( VOID, "Heat" );
     $MyData->addPoints( VOID, "Cool" );
+    $MyData->addPoints( VOID, "Temperature" );
   }
 }
+
 mysql_close( $link );
 
 // Attach the data series to the axis (by ordinal.  0 is X-axis)
 $MyData->setSerieOnAxis( "Heat", 0 );
 $MyData->setSerieOnAxis( "Cool", 0 );
+$MyData->setSerieOnAxis( "Temperature", 1 );
+$MyData->setAxisPosition( 1, AXIS_POSITION_RIGHT );
 
 // Set line style, color, and alpha blending level
 $MyData->setSerieTicks( "Heat", 0 );  // 0 is a solid line
 $serieSettings = array( "R" => 150, "G" => 50, "B" => 80, "Alpha" => 90 );
 $MyData->setPalette( "Heat", $serieSettings );
 
-//$MyData->setSerieTicks( "Cool", 0); // n is length in pixels of dashes in line
 $serieSettings = array( "R" => 50, "G" => 150, "B" => 180, "Alpha" => 90 );
 $MyData->setPalette( "Cool", $serieSettings );
 
+$serieSettings = array( "R" => 175, "G" => 75, "B" => 105, "Alpha" => 100 );
+$MyData->setPalette( "Temperature", $serieSettings );
+
 // Set names for Y-axis labels
 $MyData->setAxisName( 0, "Minutes" );
+$MyData->setAxisName( 1, "Degrees" );
 
 // Set names for X-axis labels
 $MyData->setSerieDescription( "The days" );
@@ -107,14 +156,27 @@ $myPicture->setShadow(TRUE,array("X"=>1,"Y"=>1,"R"=>50,"G"=>50,"B"=>50,"Alpha"=>
 // Define the chart area
 $myPicture->setGraphArea( 60, 60, 850, 390 );
 
-
+// Explicity set a scale for the drawing.
+$AxisBoundaries = array( 0 => array ( "Min" => $chart_runtime_min, "Max" => $chart_runtime_max ), 1 => array ( "Min" => $chart_temp_min, "Max" => $chart_temp_max ) );
 // Draw the scale
 $myPicture->setFontProperties( array( "FontName" => "lib/pChart2.1.3/fonts/pf_arma_five.ttf", "FontSize" => 6 ) );
-$Settings = array("Pos"=>SCALE_POS_LEFTRIGHT, "Mode"=>SCALE_MODE_FLOATING, "LabelingMethod"=>LABELING_ALL, "GridR"=>255, "GridG"=>255, "GridB"=>255, "GridAlpha"=>50, "TickR"=>0, "TickG"=>0, "TickB"=>0, "TickAlpha"=>50, "LabelRotation"=>0, "CycleBackground"=>1, "DrawXLines"=>1, "DrawSubTicks"=>1, "SubTickR"=>255, "SubTickG"=>0, "SubTickB"=>0, "SubTickAlpha"=>50, "DrawYLines"=>ALL);
+//$Settings = array("Pos"=>SCALE_POS_LEFTRIGHT, "Mode"=>SCALE_MODE_FLOATING, "LabelingMethod"=>LABELING_ALL, "GridR"=>255, "GridG"=>255, "GridB"=>255, "GridAlpha"=>50, "TickR"=>0, "TickG"=>0, "TickB"=>0, "TickAlpha"=>50, "LabelRotation"=>0, "CycleBackground"=>1, "DrawXLines"=>1, "DrawSubTicks"=>1, "SubTickR"=>255, "SubTickG"=>0, "SubTickB"=>0, "SubTickAlpha"=>50, "DrawYLines"=>ALL);
+$Settings = array("Pos"=>SCALE_POS_LEFTRIGHT, "Mode"=>SCALE_MODE_MANUAL, "ManualScale"=>$AxisBoundaries, "LabelingMethod"=>LABELING_ALL, "GridR"=>255, "GridG"=>255, "GridB"=>255, "GridAlpha"=>50, "TickR"=>0, "TickG"=>0, "TickB"=>0, "TickAlpha"=>50, "LabelRotation"=>0, "CycleBackground"=>1, "DrawXLines"=>1, "DrawSubTicks"=>1, "SubTickR"=>255, "SubTickG"=>0, "SubTickB"=>0, "SubTickAlpha"=>50, "DrawYLines"=>ALL);
 $myPicture->drawScale($Settings);
 
 // Define shadows under series lines
-$myPicture->setShadow( TRUE, array( "X" => 1, "Y" => 1, "R" => 0, "G" => 0, "B" => 0, "Alpha" => 40 ) );
+$myPicture->setShadow( FALSE, array( "X" => 1, "Y" => 1, "R" => 0, "G" => 0, "B" => 0, "Alpha" => 40 ) );
+
+// Draw the outdoor max temperature as a filled line chart
+$MyData->setSerieDrawable( "Heat", FALSE );
+$MyData->setSerieDrawable( "Cool", FALSE );
+$MyData->setSerieDrawable( "Temperature", TRUE );
+$myPicture->drawAreaChart();
+
+// Draw the run times as bar charts
+$MyData->setSerieDrawable( "Heat", TRUE );
+$MyData->setSerieDrawable( "Cool", TRUE );
+$MyData->setSerieDrawable( "Temperature", FALSE );
 $myPicture->drawBarChart( array("Gradient"=>1, "AroundZero"=>1) );
 
 // Write the chart legend
@@ -122,5 +184,5 @@ $myPicture->setFontProperties( array( "FontName" => "lib/pChart2.1.3/fonts/pf_ar
 $myPicture->drawLegend( 710, 412, array( "Style" => LEGEND_NOBORDER, "Mode" => LEGEND_HORIZONTAL ) );
 
 
-$myPicture->stroke();
+$myPicture->autoOutput();
 ?>
