e0e92f7c1badac7ecf80a228fcec741b57c5f382
ThermoMan <thestalwart1-tstat@yahoo.com>
2014-03-05 23:58:12 -0600

    Ooops, left the DOS/Windows version behind
    This catches up to the Unix version launcher update.

--- scripts/thermo_update_temps.bat
+++ scripts/thermo_update_temps.bat
@@ -1,2 +1,6 @@
 cd C:\xampp\htdocs\thermo2\
-C:\xampp\php\php C:\xampp\htdocs\thermo2\scripts\thermo_update_temps.php
+
+set NOW=%DATE% %TIME:~0,8%
+set NOW_CHANGED=%NOW:~0,15%0:00
+
+C:\xampp\php\php C:\xampp\htdocs\thermo2\scripts\thermo_update_temps.php "%NOW_CHANGED%"
