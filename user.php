7b8feba858f578417ed204d55a57d7e6097da19d
ThermoMan <thestalwart1-tstat@yahoo.com>
2018-07-06 18:38:35 -0500

    Start of commit for version 3 (this is VERY alpha)
    Update to user structure reflecting better DB normalization.

--- user.php
+++ user.php
@@ -1,5 +1,5 @@
 <?php
-  require_once( 'common.php' );
+require_once( 'common.php' );
 
 /**
   * User registration/login loosely based on http://www.codingcage.com/2015/04/php-login-and-registration-script-with.html
@@ -13,9 +13,12 @@ class USER{
   private $session;
 
   public function __construct( $uname = null, $session = null ){
+    global $util;
+$util::logDebug( 'user.php: construct 0' );
     $this->database = new Database();
     $db = $this->database->dbConnection();
     $this->conn = $db;
+//$util::logDebug( 'user.php: construct 1' );
 
     if( $uname == null && $session == null && $this->isLoggedIn() ){
       $this->uname = $_SESSION[ 'user_name' ];
@@ -26,21 +29,63 @@ class USER{
       $this->session = $session;
     }
 
+//$util::logDebug( 'user.php: construct 2' );
     if( $this->uname != null && $this->session != null ){
       // Put user Thermostat(s) in the list.
-      $stmt = $this->conn->prepare( "SELECT stat.id, stat.tstat_uuid, stat.model, stat.fw_version, stat.wlan_fw_version, stat.ip, stat.name, stat.description, stat.user_id, stat.zip_code, stat.timezone FROM {$this->database->table_prefix}thermostats AS stat, {$this->database->table_prefix}users AS user WHERE user.user_id = stat.user_id AND user.user_name = :uname" );
+//$util::logDebug( 'user.php: construct 2a' );
+//      $sql = "SELECT stat.thermostat_id, stat.tstat_uuid, stat.model, stat.fw_version, stat.wlan_fw_version, stat.ip, stat.name, stat.description, stat.user_id, stat.zip_code, stat.timezone FROM {$this->database->table_prefix}thermostats AS stat, {$this->database->table_prefix}users AS user WHERE user.user_id = stat.user_id AND user.user_name = :uname";
+      $sql = "
+SELECT stat.thermostat_id
+      ,stat.tstat_uuid
+      ,stat.model
+      ,stat.fw_version
+      ,stat.wlan_fw_version
+      ,stat.ip
+      ,stat.name
+      ,stat.description
+      ,stat.user_id
+      ,loc.location_string
+      ,loc.timezone
+FROM {$this->database->table_prefix}users AS user
+    ,{$this->database->table_prefix}thermostats AS stat
+    ,{$this->database->table_prefix}locations AS loc
+WHERE user.user_name = :uname
+  AND stat.user_id = user.user_id
+  AND loc.location_id = stat.location_id";
+
+      $stmt = $this->conn->prepare( $sql );
+//$util::logDebug( 'user.php: construct 2b' );
       $stmt->bindParam( ':uname', $this->uname );
+//$util::logDebug( 'user.php: construct 2c' );
       $stmt->execute();
+//$util::logDebug( 'user.php: construct 2d' );
 
       $this->thermostats = $stmt->fetchAll( PDO::FETCH_ASSOC );
+//$util::logDebug( 'user.php: construct 2e' );
 
       // Put user TED 5000(s) in the list.
-      $stmt = $this->conn->prepare( "SELECT mtu.user_id, mtu.mtu_id, mtu.ip, mtu.name, mtu.description FROM elec__meters AS mtu, {$this->database->table_prefix}users AS user WHERE user.user_id = mtu.user_id AND user.user_name = :uname" );
+//      $sql2 = "SELECT mtu.user_id, mtu.mtu_id, mtu.ip, mtu.name, mtu.description FROM elec__meters AS mtu, {$this->database->table_prefix}users AS user WHERE user.user_id = mtu.user_id AND user.user_name = :uname";
+      $sql2 = "
+SELECT mtu.mtu_id
+      ,mtu.mtu
+      ,mtu.ip
+      ,mtu.name
+      ,mtu.description
+      ,loc.location_string
+      ,loc.timezone
+FROM {$this->database->table_prefix}users AS user
+    ,{$this->database->table_prefix}meters AS mtu
+    ,{$this->database->table_prefix}locations AS loc
+WHERE user.user_name = :uname
+  AND mtu.user_id = user.user_id
+  AND loc.location_id = mtu.location_id";
+      $stmt = $this->conn->prepare( $sql2 );
       $stmt->bindParam( ':uname', $this->uname );
       $stmt->execute();
 
       $this->TED5000_Gateways = $stmt->fetchAll( PDO::FETCH_ASSOC );
     }
+$util::logDebug( 'user.php: construct 3' );
   }
 
   public function getName(){
@@ -59,7 +104,8 @@ class USER{
     return false;
   }
 
-  // Why is this called runSQL?  It doesn't run the SQL....
+  // QQQ Why is this called runSQL?  It doesn't run the SQL....
+  // QQQ Probably ought to delete this.
   public function runQuery( $sql ){
     $stmt = $this->conn->prepare( $sql );
     return $stmt;
@@ -72,6 +118,7 @@ class USER{
 
       $stmt = $this->conn->prepare( "INSERT INTO {$this->database->table_prefix}users( user_name, email, pass, validation_key ) VALUES( :uname, :umail, :upass, :key )" );
 
+// QQQ Need to generate a real key before used in production environment.
 $key = '12345';
       $stmt->bindParam( ':uname', $uname );
       $stmt->bindParam( ':umail', $umail );
@@ -80,10 +127,8 @@ $key = '12345';
 
       $stmt->execute();
 
-      $message = "Please click this <a href='https://freitag.theinscrutable.us/thermo2/confirm.php?key=$key'>link</a> to confirm your registation.";
+      $message = "Please click this <a href='URL TO SERVER/thermo2/confirm.php?key=$key'>link</a> to confirm your registation.";
       $util::send_mail( $umail, $message, 'User ID registration email' );
-
-
 
 //      return $stmt;
       return true;
@@ -187,15 +232,28 @@ $key = '12345';
     return false;
   }
 
-  public function redirect( $url ){
-    header( "Location: $url" );
-    exit;
-  }
-
   public function doLogout(){
+    global $util;
+$util::logDebug( 'USER->doLogout' );
     session_destroy();
     unset( $_SESSION[ 'user_session' ] );
+
+    // Taken from: http://php.net/manual/en/function.session-destroy.php
+    // Unset all of the session variables.
+    $_SESSION = array();
+
+    // If it's desired to kill the session, also delete the session cookie.
+    // Note: This will destroy the session, and not just the session data!
+    if( ini_get( 'session.use_cookies' ) ){
+        $params = session_get_cookie_params();
+        setcookie( session_name(), '', time() - 42000,
+            $params[ 'path'], $params[ 'domain' ],
+            $params[ 'secure'], $params[ 'httponly' ]
+        );
+    }
+
     return true;
   }
+
 }
 ?>
