4eb3d2b16235910eeeb799233adb22bc5e7b1763
ThermoMan <thestalwart1-tstat@yahoo.com>
2018-07-06 18:43:25 -0500

    Start of commit for version 3 (this is VERY alpha)
    DB has been more normalized.
    true/false started acting screwy, replaced with 1/0 instead of trying to debug PHP (I skipped from an early PHP 5 to recent PHP 7)

--- scripts/thermo_update_status.php
+++ scripts/thermo_update_status.php
@@ -4,17 +4,7 @@ require(dirname(__FILE__).'/../common.php');
 
 global $util;
 
-$util::logDebug( 'status: Start.' );
-
-/**
-  * (this note may be obsolete)
-  * Because of different structure and use of hvac_status table, migration from old version to new version is non-trivial.
-  * The best bet is to set up the new code running in parallel and then once it's verified working in your environment
-  *  shut down the old data collectors, export your old historic temperature data and then import into the new structure.
-  *  Then drop your old tables and delete your old install
-  *
-  * Updated pretty much directly from phareous code fork.
-  */
+$util::logInfo( 'status: Start.' );
 
 /**
   * This script periodically (once a minute) queries each thermostat and writes the status into
@@ -34,34 +24,38 @@ $util::logDebug( 'status: Start.' );
   */
 
 // Bandaid to keep things moving
-$util::logDebug( 'status: 0' );
+//$util::logDebug( 'status: 0' );
 $database = new Database();
-
 $pdo = $database->dbConnection();
 if( is_null( $pdo ) ){
-$util::logError( 'status: Cannot connect to DB, bailing.  Really ought to email someone about this.' );
-die();
+  $util::logError( 'status: Cannot connect to DB, bailing.  Really ought to email someone about this.' );
+  die();
 }
 //$util::logDebug( 'status: A' );
 try{
 //$util::logDebug( 'status: B' );
 //$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ERRMODE_WARNING );
 
-  $sql = "SELECT stat.id, stat.tstat_uuid, stat.model, stat.fw_version, stat.wlan_fw_version, stat.ip, stat.name, stat.description, stat.user_id, stat.zip_code, stat.timezone FROM {$database->table_prefix}thermostats AS stat";
+  $sql = "
+SELECT stat.thermostat_id
+      ,stat.tstat_uuid
+      ,stat.ip
+      ,stat.user_id
+ FROM {$database->table_prefix}thermostats AS stat";
 //$util::logDebug( "status: C --->$sql<---" );
   $stmt = $pdo->prepare( $sql );
 //$util::logDebug( 'status: D' );
 }
 catch( Exception $e ){
-//$util::logDebug( 'status: e' );
+//$util::logDebug( 'status: E' );
   $util::logError( 'status: DB Exception while preparing SQL: ' . $e->getMessage() );
   die();
 }
-//$util::logDebug( 'status: f' );
+//$util::logDebug( 'status: F' );
 $stmt->execute();
-//$util::logDebug( 'status: g' );
+//$util::logDebug( 'status: G' );
 $allThermostats = $stmt->fetchAll( PDO::FETCH_ASSOC );
-//$util::logDebug( 'status: h' );
+//$util::logDebug( 'status: H' );
 
 try{
   // Query to location info about the thermostat.  Might find nothing if this is the first time.
@@ -72,22 +66,28 @@ try{
   $sql = "INSERT INTO {$database->table_prefix}hvac_status( tstat_uuid, date, start_date_heat, start_date_cool, start_date_fan, heat_status, cool_status, fan_status ) VALUES( ?, ?, ?, ?, ?, ?, ?, ? )";
   $insertStatInfo = $pdo->prepare( $sql );
 
-  // Modify the thermostat data
-  $sql = "UPDATE {$database->table_prefix}thermostats SET tstat_uuid = ?, description = ?, model = ?, fw_version = ?, wlan_fw_version = ? WHERE id = ?";
-  $updateStatInfo = $pdo->prepare( $sql );
-
-  $sql = "UPDATE {$database->table_prefix}hvac_status SET date = ?, start_date_heat = ?, start_date_cool = ?, start_date_fan = ?, heat_status = ?, cool_status = ?, fan_status = ? WHERE tstat_uuid = ?";
+  $sql = "
+UPDATE {$database->table_prefix}hvac_status
+   SET date = ?
+      ,start_date_heat = ?
+      ,start_date_cool = ?
+      ,start_date_fan = ?
+      ,heat_status = ?
+      ,cool_status = ?
+      ,fan_status = ?
+ WHERE tstat_uuid = ?";
   $updateStatStatus = $pdo->prepare( $sql );
 
-  $sql = "INSERT INTO {$database->table_prefix}hvac_cycles( tstat_uuid, system, start_time, end_time ) VALUES( ?, ?, ?, ? )";
+  $sql = "INSERT INTO {$database->table_prefix}hvac_cycles( thermostat_id, system, start_time, end_time ) VALUES( ?, ?, ?, ? )";
   $cycleInsert = $pdo->prepare( $sql );
 
   // Query to retrieve prior setpoint.  Might find nothing if this is the first time.
-  $sql = "SELECT set_point FROM {$database->table_prefix}setpoints WHERE id=? ORDER BY switch_time DESC LIMIT 1";
+  $sql = "SELECT set_point FROM {$database->table_prefix}setpoints WHERE thermostat_id=? ORDER BY switch_time DESC LIMIT 1";
   $getPriorSetPoint = $pdo->prepare( $sql );
 
-  $sql = "INSERT INTO {$database->table_prefix}setpoints( id, set_point, switch_time ) VALUES( ?, ?, ? )";
+  $sql = "INSERT INTO {$database->table_prefix}setpoints( thermostat_id, set_point, switch_time ) VALUES( ?, ?, ? )";
   $insertSetPoint = $pdo->prepare( $sql );
+//$util::logDebug( 'status: I' );
 }
 catch( Exception $e ){
   $util::logError( 'status: DB Exception while preparing SQL: ' . $e->getMessage() );
@@ -97,7 +97,8 @@ global $lockFile;
 
 $now = date( 'Y-m-d H:i:00' );
 foreach( $allThermostats as $thermostatRec ){
-  $lockFileName = $lockFile . $thermostatRec['id'];
+//$util::logDebug( 'status: J' );
+  $lockFileName = $lockFile . $thermostatRec['thermostat_id'];
   $lock = @fopen( $lockFileName, 'w' );
   if( !$lock ){
     $util::logError( "status: Could not write to lock file $lockFileName" );
@@ -107,7 +108,6 @@ foreach( $allThermostats as $thermostatRec ){
   if( flock($lock, LOCK_EX) ){
     try{
       // Query thermostat info
-      //$util::logDebug( "status: Connecting to Thermostat ID = ({$thermostatRec['id']})  uuid  = ({$thermostatRec['tstat_uuid']}) ip = ({$thermostatRec['ip']}) name = ({$thermostatRec['name']})" );
       $stat = new Stat( $thermostatRec );
 
       /**
@@ -120,104 +120,97 @@ foreach( $allThermostats as $thermostatRec ){
         * Should compare the detected uuid back to the thermostat table record
         * On match, do nothing.  On 'no match', make sure it matches no other record too and then update existing record (and log it)
         */
+//$util::logDebug( 'status: K' );
       $stat->getSysInfo();
+//$util::logDebug( 'status: L' );
+
+// there is an update to tlib - this error check no longer helps.  tlib throws an exception if it got no data
       if( $stat->connectOK != 0 ){
-        $log->logWarn( "status: connectOK is not zero!  We should not proceed!  connectOK = ($stat->connectOK).  Perhaps for a macro level retry even though the micro level retry already failed?" );
+//$util::logDebug( 'status: M' );
+        $util::logWarn( "status: connectOK is not zero!  We should not proceed!  connectOK = ($stat->connectOK).  Perhaps for a macro level retry even though the micro level retry already failed?" );
         // An error here may not need to be fatal, but if it worked, should verify that stat uuid matches expected uuid in DB
         // If it does not match expected, does it match ANY?  Email admin if the user ID for matched does not match user ID of expected!! (possible hacking?)
       }
 
-/* 2014-04-28 commented out.  Maybe only check model once every 30 minutes in the other routine?
-      // Perhaps only check this info one time per day or when the reported uuid is not same as stored uuid
-      $stat->getModel();
-      if( $stat->connectOK != 0 )
-      { // An error here is non-fatal, simply decline to use this info
-        $util::logError( 'status: Thermostat failed to respond with model number.' );
-      }
-*/
-
-/* 2014-04-28 commented out.  Maybe only check name once every 30 minutes in the other routine?
-      $stat->getSysName();
-      if( $stat->connectOK != 0 )
-      { // An error here is non-fatal, simply decline to use this info
-        $util::logError( 'status: Thermostat failed to respond with system info.' );
-      }
-*/
-// Declining to update the thermostat info for now because a 0 or null in the uuid will screw up record collection the next time for this stat
-      //$util::logDebug( "status: Updating thermostat record {$thermostatRec['id']}: UUID $stat->uuid DESC $stat->sysName MDL $stat->model FW $stat->fw_version WLANFW $stat->wlan_fw_version" );
-      //Update thermostat info in DB
-      //$updateStatInfo->execute(array( $stat->uuid , $stat->sysName, $stat->model, $stat->fw_version, $stat->wlan_fw_version, $thermostatRec['id']));
-
       // Get thermostat state (time, temp, mode, hold, override)
+//$util::logDebug( 'status: N' );
       $stat->getStat();
+//$util::logDebug( 'status: O' );
       if( $stat->connectOK == 0 ){
-        $heatStatus = ($stat->tstate == 1) ? true : false;
-        $coolStatus = ($stat->tstate == 2) ? true : false;
-        $fanStatus  = ($stat->fstate == 1) ? true : false;
+//$util::logDebug( 'status: P' );
+        $heatStatus = ($stat->tstate == 1) ? 1 : 0;
+        $coolStatus = ($stat->tstate == 2) ? 1 : 0;
+        $fanStatus  = ($stat->fstate == 1) ? 1 : 0;
+//$util::logDebug( 'status: P0 RAW $stat->tstate ' . $stat->tstate );
+//$util::logDebug( 'status: P1 $heatStatus ' . $heatStatus );
+//$util::logDebug( 'status: P2 $coolStatus ' . $coolStatus );
+//$util::logDebug( 'status: P3 $fanStatus ' . $fanStatus );
 
         // Get current setPoint from thermostat
         // t_heat or t_cool may not exist if thermostat is running in battery mode (will it even talk on WiFi if the power is out?)
         $setPoint = ($stat->tmode == 1) ? $stat->t_heat : $stat->t_cool;
       }
       else{
+//$util::logDebug( 'status: Q' );
         $util::logError( 'status: Thermostat failed to respond with present status' );
         // Instead of continue, I should throw a thermostat exception!
         continue; // Cannot continue workting on this thermostat, try the next one in the list.
       }
+//$util::logDebug( 'status: R' );
 
       // Get prior setPoint from database
-      $getPriorSetPoint->execute(array($thermostatRec['id']));
+      $getPriorSetPoint->execute(array($thermostatRec['thermostat_id']));
       $priorSetPoint = $getPriorSetPoint->fetchColumn();
+//$util::logDebug( 'status: S' );
 
       // Get prior state info from DB
       $priorStartDateHeat = null;
       $priorStartDateCool = null;
       $priorStartDateFan = null;
-      $priorHeatStatus = false;
-      $priorCoolStatus = false;
-      $priorFanStatus = false;
+      $priorHeatStatus = 0; // For some reason false stopped working for me.  I had to switch to 0.  So I also changed true to 1 even though it was still working.  Seriously WTF?
+      $priorCoolStatus = 0;
+      $priorFanStatus = 0;
 
 // Possibly controversial code change here.  This assumes the uuid never changes once set.
       // Look up thermostat previous status based on the uuid (uuid as reported by the thermostat - BAD IDEA)
-      $getStatInfo->execute( array( $stat->uuid ) );
-
-      // Look up thermostat previous status based on the uuid (uuid as expected from DB)
-//      $getStatInfo->execute( array( $thermostatRec['tstat_uuid'] ) );
-// I think looking for a changed uuid can reasonable be done ONCE per day (or per week!) and use the thermostat_id as the key in the temperatures table
-// DRAT.  Have to use the uuid provided by the stat because this might be the FIRST contact for a NEW stat
-
+      $getStatInfo->execute( array( $stat->thermostat_id ) );
+//$util::logDebug( 'status: T' );
 
       if( $getStatInfo->rowCount() < 1 ){
         // not found - this is the first time connection for this thermostat
-        $util::logDebug( 'status: I think I found a new/different thermostat at the specified URL' );
+//$util::logDebug( 'status: I think I found a new/different thermostat at the specified URL' );
 // Perhaps key in on this logic to drive the deep query for the stat??
         $startDateHeat = ($heatStatus) ? $now : null;
         $startDateCool = ($coolStatus) ? $now : null;
         $startDateFan = ($fanStatus) ? $now : null;
 
-        $util::logDebug( "status: Inserting record for a brand new never before seen thermostat with time = ($now) H $heatStatus C $coolStatus F $fanStatus SDH $startDateHeat SDC $startDateCool SDF $startDateFan for UUID $stat->uuid" );
+//$util::logDebug( "status: Inserting record for a brand new never before seen thermostat with time = ($now) H $heatStatus C $coolStatus F $fanStatus SDH $startDateHeat SDC $startDateCool SDF $startDateFan for UUID $stat->uuid" );
 // Have been getting really lucky here.  Communicatiuon errors with the stat leave NULLs in that do not match existing stats.
 // Leading to false idea that the stat we're talking to is new (because existing stats not equal NULL on uuid)
 // So attempt to insert record for new stat, but fail because no NULLs allowed in key columns.  So no new record.  Lucky!
 // Proper fix is to abort when the stat was not able to be reached.
 // Also proper fix includes not trying to insert NULLs and catching SQL errors when something happens like that (and log it)
-        $insertStatInfo->execute( array( $stat->uuid, $now, $startDateHeat, $startDateCool, $startDateFan, $heatStatus, $coolStatus, $fanStatus ) );
+        $insertStatInfo->execute( array( $stat->thermostat_id, $now, $startDateHeat, $startDateCool, $startDateFan, $heatStatus, $coolStatus, $fanStatus ) );
 
-        $util::logDebug( "setpoints: Inserting record for a brand new never before seen thermostat with setpoint=$setPoint, time=($now) " );
-        $insertSetPoint->execute( array( $thermostatRec['id'], $setPoint, $now ) );
+//$util::logDebug( "setpoints: Inserting record for a brand new never before seen thermostat with setpoint=$setPoint, time=($now) " );
+        $insertSetPoint->execute( array( $thermostatRec['thermostat_id'], $setPoint, $now ) );
       }
       else{
+        $hvacStatusChanged = 0;
         while( $row = $getStatInfo->fetch( PDO::FETCH_ASSOC ) ){
           // This SQL had _BETTER_ pull only one row or else there is a data integrity problem!
           // and without an ORDER BY on the SELECT there is no way to know you're geting the same row from this each time
           $priorStartDateHeat = $row['start_date_heat'];
           $priorStartDateCool = $row['start_date_cool'];
           $priorStartDateFan = $row['start_date_fan'];
-          $priorHeatStatus = (bool)$row['heat_status'];
-          $priorCoolStatus = (bool)$row['cool_status'];
-          $priorFanStatus = (bool)$row['fan_status'];
+          $priorHeatStatus = $row['heat_status'];
+          $priorCoolStatus = $row['cool_status'];
+          $priorFanStatus = $row['fan_status'];
+//$util::logDebug( 'status: T1 $priorHeatStatus ' . $heatStatus . ' from ' . $row['heat_status'] );
+//$util::logDebug( 'status: T2 $priorCoolStatus ' . $coolStatus . ' from ' . $row['cool_status'] );
+//$util::logDebug( 'status: T3 $priorFanStatus ' . $fanStatus . ' from ' . $row['fan_status'] );
         }
-        //$util::logDebug( "status:  uuid = ($stat->uuid) GOT PRIOR STATE H $priorHeatStatus C $priorCoolStatus F $priorFanStatus SDH $priorStartDateHeat SDC $priorStartDateCool SDC $priorStartDateFan" );
+//$util::logDebug( "status:  uuid = ($stat->uuid) // $thermostatRec[tstat_uuid] GOT PRIOR STATE H $priorHeatStatus C $priorCoolStatus F $priorFanStatus SDH $priorStartDateHeat SDC $priorStartDateCool SDC $priorStartDateFan" );
 
         // update start dates if the cycle just started
         $newStartDateHeat = (!$priorHeatStatus && $heatStatus) ? $now : $priorStartDateHeat;
@@ -226,28 +219,37 @@ foreach( $allThermostats as $thermostatRec ){
 
         // if status has changed from on to off, update hvac_cycles
         if( $priorHeatStatus && !$heatStatus ){
-          //$util::logDebug( "status: uuid = ($stat->uuid) Finished Heat Cycle - Adding Hvac Cycle Record for $stat->uuid 1 $priorStartDateHeat $now" );
-          $cycleInsert->execute( array( $stat->uuid, 1, $priorStartDateHeat, $now ) );
+//$util::logDebug( "status: $stat->uuid // $thermostatRec[tstat_uuid] Finished Heat Cycle - Adding Hvac Cycle Record for $stat->thermostat_id 1 $priorStartDateHeat $now" );
+          $cycleInsert->execute( array( $stat->thermostat_id, 1, $priorStartDateHeat, $now ) );
           $newStartDateHeat = null;
+          $hvacStatusChanged = 1;
         }
         if( $priorCoolStatus && !$coolStatus ){
-          //$util::logDebug( "status: $stat->uuid Finished Cool Cycle - Adding Hvac Cycle Record for $stat->uuid 2 $priorStartDateCool $now" );
-          $cycleInsert->execute( array( $stat->uuid, 2, $priorStartDateCool, $now ) );
+//$util::logDebug( "status: $stat->uuid // $thermostatRec[tstat_uuid] Finished Cool Cycle - Adding Hvac Cycle Record for $stat->thermostat_id 2 $priorStartDateCool $now" );
+          $cycleInsert->execute( array( $stat->thermostat_id, 2, $priorStartDateCool, $now ) );
           $newStartDateCool = null;
+          $hvacStatusChanged = 1;
         }
         if( $priorFanStatus && !$fanStatus ){
-          //$util::logDebug( "status: $stat->uuid Finished Fan Cycle - Adding Hvac Cycle Record for $stat->uuid 3 $priorStartDateFan $now" );
-          $cycleInsert->execute( array( $stat->uuid, 3, $priorStartDateFan, $now ) );
+//$util::logDebug( "status: $stat->uuid // $thermostatRec[tstat_uuid] Finished Fan Cycle - Adding Hvac Cycle Record for $stat->thermostat_id 3 $priorStartDateFan $now" );
+          $cycleInsert->execute( array( $stat->thermostat_id, 3, $priorStartDateFan, $now ) );
           $newStartDateFan = null;
+          $hvacStatusChanged = 1;
         }
-        // update the status table
-        //$util::logDebug( "status: Updating record with $now SDH $newStartDateHeat SDC $newStartDateCool SDF $newStartDateFan H $heatStatus C $coolStatus F $fanStatus for UUID $stat->uuid" );
-        $updateStatStatus->execute( array( $now, $newStartDateHeat, $newStartDateCool, $newStartDateFan, $heatStatus, $coolStatus, $fanStatus, $stat->uuid ) );
-
+//$util::logDebug( 'status: U $hvacStatusChanged is ' . $hvacStatusChanged );
+        if( $hvacStatusChanged == 1 ){
+          // update the status table
+//$util::logDebug( "status: Updating record with $now SDH $newStartDateHeat SDC $newStartDateCool SDF $newStartDateFan H $heatStatus C $coolStatus F $fanStatus for UUID $stat->uuid" );
+          $updateStatStatus->execute( array( $now, $newStartDateHeat, $newStartDateCool, $newStartDateFan, $heatStatus, $coolStatus, $fanStatus, $stat->uuid) );
+//$util::logDebug( 'status: I think there were (' . $updateStatStatus->rowCount() . ') rows updated (expecting 1).' );
+        }
+else{
+//$util::logDebug( 'status: No change so no update' );
+}
         //Update the setpoints table
         if( $setPoint != $priorSetPoint ){
           $util::logDebug( "status: Inserting changed setpoint record SP=$setPoint, old=($priorSetPoint), time=($now) " );
-          $insertSetPoint->execute( array( $thermostatRec['id'], $setPoint, $now ) );
+          $insertSetPoint->execute( array( $thermostatRec['thermostat_id'], $setPoint, $now ) );
         }
       }
     }
@@ -257,10 +259,11 @@ foreach( $allThermostats as $thermostatRec ){
     flock( $lock, LOCK_UN );
   }
   else{
-    $util::logError( "status: Couldn't get file lock for thermostat {$thermostatRec['id']}" );
+    $util::logError( "status: Couldn't get file lock for thermostat {$thermostatRec['thermostat_id']}" );
   }
   fclose( $lock );
 }
-$util::logDebug( 'status: End.  Execution time was ' . (microtime(true) - $start_time) . ' seconds.' );
+//$database->disconnect();
+$util::logInfo( 'status: End.  Execution time was ' . (microtime(true) - $start_time) . ' seconds.' );
 
 ?>
