a3cf69eb754783a70456c56aa533675b9d947f31
ThermoMan <thestalwart1-tstat@yahoo.com>
2018-07-07 01:48:57 -0500

    Start of commit for version 3 (this is VERY alpha)
    Detail front end and back end.  No longer using pChart.
    BE AWARE - this has location hard-coded for now (It's alpha after all)

--- daily_detail.php
+++ daily_detail.php
@@ -4,103 +4,78 @@
   if( ! $util::checkThermostat( $user ) ) return;
 ?>
 
-<script type='text/javascript'>
-/**
-  * chart is one of 'daily' or 'history'
-  * sytle is one of 'chart' or 'table'
-  *
-  */
-function display_chart( chart, style ){
-  var chart_target;
-  var table_flag = '';
-
-  if( chart == 'daily' && style == 'chart' ){
-    chart_target = document.getElementById( 'daily_temperature_chart' );
-    chart_target.src = 'images/daily_temperature_placeholder.png';  // Redraw the placekeeper while the chart is rendering
-    // By using setTimeout we can separate the drawing of the placeholder image from the actual chart such that the browser will always draw the placeholder
-    setTimeout( function(){ display_chart_build_and_display( chart, style, 'false', chart_target ); }, 500 );
-  }
-  else if( chart == 'daily' && style == 'table' ){
-
-    table_flag = 'table_flag=true';
-    chart_target = document.getElementById( 'daily_temperature_table' );
-    chart_target.innerHTML = '';
-    display_chart_build_and_display( chart, style, table_flag, chart_target );
-  }
-  else{
-    alert( 'You asked for '+chart+' and '+style+' and I do not know how to do that (yet).' );
-    return;
-  }
+<style>
+#chartdiv {
+  width: 100%;
+  height: 500px;
 }
+</style>
+<script type='text/javascript' src='../common/js/amcharts/amcharts.js'></script>
+<script type='text/javascript' src='../common/js/amcharts/serial.js'></script>
 
-
-function display_chart_build_and_display( chart, style, table_flag, chart_target ){
-//  var show_thermostat_id       = 'id='                          + document.getElementById( 'chart.daily.thermostat' ).value;
-  var daily_setpoint_selection = 'chart.daily.setpoint='        + document.getElementById( 'chart.daily.setpoint' ).checked;
-//  var daily_source_selection   = 'chart.daily.source='          + document.getElementById( 'chart.daily.source' ).value;
-//  var daily_interval_length    = 'chart.daily.interval.length=' + document.getElementById( 'chart.daily.interval.length' ).value;
-//  var daily_interval_group     = 'chart.daily.interval.group='  + document.getElementById( 'chart.daily.interval.group' ).value;
-//  var daily_to_date_string     = 'chart.daily.toDate='          + document.getElementById( 'chart.daily.toDate' ).value;
-  var show_heat_cycle_string   = 'chart.daily.showHeat='        + document.getElementById( 'chart.daily.showHeat' ).checked;
-  var show_cool_cycle_string   = 'chart.daily.showCool='        + document.getElementById( 'chart.daily.showCool' ).checked;
-  var show_fan_cycle_string    = 'chart.daily.showFan='         + document.getElementById( 'chart.daily.showFan' ).checked;
+<script type='text/javascript'>
+var rawData = [];
+function display_chart(){
+  // QQQ ALL arguments need a name review.  1) consistency  2) brevity  3) comprehension
+  var daily_setpoint_selection = 'chart.daily.setpoint='            + document.getElementById( 'chart.daily.setpoint' ).checked;
+  var show_heat_cycle_string   = 'chart.daily.showHeat='            + document.getElementById( 'chart.daily.showHeat' ).checked;
+  var show_cool_cycle_string   = 'chart.daily.showCool='            + document.getElementById( 'chart.daily.showCool' ).checked;
+  var show_fan_cycle_string    = 'chart.daily.showFan='             + document.getElementById( 'chart.daily.showFan' ).checked;
   var show_outdoor_humid_str   = 'chart.daily.showOutdoorHumidity=' + document.getElementById( 'chart.daily.showOutdoorHumidity' ).checked;
   var show_indoor_humid_str    = 'chart.daily.showIndoorHumidity='  + document.getElementById( 'chart.daily.showIndoorHumidity' ).checked;
 
-  var show_thermostat_id       = 'id='                              + $( '#chart\\.daily\\.thermostat' ).val();
-//  var daily_setpoint_selection = 'chart.daily.setpoint='            + $( '#chart\\.daily\\.setpoint' ).checked;
+  var show_thermostat_id       = 'thermostat_id='                   + $( '#chart\\.daily\\.thermostat' ).val();
   var daily_source_selection   = 'chart.daily.source='              + $( '#chart\\.daily\\.source' ).val();
   var daily_interval_length    = 'chart.daily.interval.length='     + $( '#chart\\.daily\\.interval\\.length' ).val();
   var daily_interval_group     = 'chart.daily.interval.group='      + $( '#chart\\.daily\\.interval\\.group' ).val();
   var daily_to_date_string     = 'chart.daily.toDate='              + $( '#chart\\.daily\\.toDate' ).val();
-//  var show_heat_cycle_string   = 'chart.daily.showHeat='            + $( '#chart\\.daily\\.showHeat' ).checked;
-//  var show_cool_cycle_string   = 'chart.daily.showCool='            + $( '#chart\\.daily\\.showCool' ).checked;
-//  var show_fan_cycle_string    = 'chart.daily.showFan='             + $( '#chart\\.daily\\.showFan' ).checked;
-//  var show_outdoor_humid_str   = 'chart.daily.showOutdoorHumidity=' + $( '#chart\\.daily\\.showOutdoorHumidity' ).checked;
-//  var show_indoor_humid_str    = 'chart.daily.showIndoorHumidity='  + $( '#chart\\.daily\\.showIndoorHumidity' ).checked;
-
 
   // Browsers are very clever with image caching. In this case it breaks the web page function.
   var no_cache_string = 'nocache=' + Math.random();
 
   var url_string = '';
-  if( chart == 'daily' ){
-    url_string = 'daily_detail_dl.php?user=<?php echo $user->getName() ?>&session=<?php echo $user->getSession() ?>';
-  }
-  else if( chart == 'history' ){
-    // This space intentionally left blank (actually I don't recall why it was left blank, but everything seems to work)
-  }
+  url_string = 'daily_detail_dl?user=<?php echo $user->getName() ?>&session=<?php echo $user->getSession() ?>';
 
-  url_string = url_string + '&' + show_thermostat_id + '&' + daily_source_selection + '&' + daily_setpoint_selection + '&' + table_flag + '&' +
+  url_string = url_string + '&' + show_thermostat_id + '&' + daily_source_selection + '&' + daily_setpoint_selection + '&' +
                daily_interval_length  + '&' + daily_interval_group   + '&' + daily_to_date_string  + '&' +
                show_heat_cycle_string + '&' + show_cool_cycle_string + '&' + show_fan_cycle_string + '&' +
                show_outdoor_humid_str + '&' + show_indoor_humid_str  + '&' +
                no_cache_string;
 
-  if( style == 'chart' ){
-    chart_target.src = url_string;
-  }
-  else if( style == 'table' ){
-    // Right now it assumes the DAILY table.  Fix that later
-    // Size should be in the CSS file?
+  // Make ajax call to data source
+  $.getJSON( url_string, function( data ){
+    rawData = [];   // Reset data array i ncase this is a re-run
+    var min = 100;  // Set chart Y axis boundary flags so they are guaranteed to move
+    var max = 0;
 
-    // Gosh this iframe is a whole load of overkill and I hate it.
-    chart_target.innerHTML = '<iframe src="'+url_string+'" height="100" width="530"></iframe>';
+debugger;
+    $.each( data.answer[ 'indoorTemp' ], function( key, value ){
+      if( $.isNumeric( value ) && value > max ) max = value;
+      else if( $.isNumeric( value ) && value < min ) min = value;
+      rawData.push( {"date": key, "value": value } );
+    });
+/* Need to programmatically add or delete sections in the chart data structure
+    $.each( data.answer[ 'outdoorTemp' ], function( key, value ){
+      if( $.isNumeric( value ) && value > max ) max = value;
+      else if( $.isNumeric( value ) && value < min ) min = value;
+      rawData.push( {"date": key, "value": value } );
+    });
+*/
 
-//    return url_string;
-  }
+    max = (Math.ceil( max / 10 ) * 10) + 10;    // Widen to nearest ten degrees upper and lower bound
+    min = (Math.floor( min / 10 ) * 10) - 10;
+    chart.valueAxes[0].maximum = max;
+    chart.valueAxes[0].minimum = min;
+    chart.dataProvider = rawData;
+    chart.validateData();
+  });
+
 }
 
 
-/**
-  * Save the value of the checkbox for later - and update the chart with the new value
-  */
-function toggle_daily_flag( flag ){
-  setCookie( flag, document.getElementById(flag).checked );
-}
 </script>
 
-<input type='button' onClick='javascript: display_chart( "daily", "chart" );' value='Show'>
+<input type='button' onClick='javascript: display_chart();' value='Show'>
 
 <select id='chart.daily.thermostat'>
 <?php
@@ -108,9 +83,9 @@ function toggle_daily_flag( flag ){
 ?>
   <option
 <?php
-    if( $id == $thermostatRec['id'] ) print( ' selected ' );
+    if( $id == $thermostatRec['thermostat_id'] ) print( ' selected ' );
 ?>
-    value='<?php print( $thermostatRec['id'] ); ?>'><?php print( $thermostatRec['name'] ); ?> </option>
+    value='<?php print( $thermostatRec['thermostat_id'] ); ?>'><?php print( $thermostatRec['name'] ); ?> </option>
 <?php
   }
 ?>
@@ -122,22 +97,20 @@ function toggle_daily_flag( flag ){
   <option value='2' selected>Both</option>
 </select>
 
-&nbsp;<input type='checkbox' id='chart.daily.setpoint' name='chart.daily.setpoint' onChange='javascript: toggle_daily_flag( "chart.daily.setpoint" );'/> Set Point
+&nbsp;<input type='checkbox' id='chart.daily.setpoint' name='chart.daily.setpoint'/> Set Point
 
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; showing Heat<input type='checkbox' id='chart.daily.showHeat' name='chart.daily.showHeat' onChange='javascript: toggle_daily_flag( "chart.daily.showHeat" );'/>
-&nbsp;Cool<input type='checkbox' id='chart.daily.showCool' name='chart.daily.showCool' onChange='javascript: toggle_daily_flag( "chart.daily.showCool" );'/>
-&nbsp;Fan<input type='checkbox' id='chart.daily.showFan'  name='chart.daily.showFan'  onChange='javascript: toggle_daily_flag( "chart.daily.showFan" );'/> cycles
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; showing Heat<input type='checkbox' id='chart.daily.showHeat' name='chart.daily.showHeat'/>
+&nbsp;Cool<input type='checkbox' id='chart.daily.showCool' name='chart.daily.showCool'/>
+&nbsp;Fan<input type='checkbox' id='chart.daily.showFan'  name='chart.daily.showFan'/> cycles
 
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; humidity Indoor<input type='checkbox' id='chart.daily.showIndoorHumidity' name='chart.daily.showIndoorHumidity' onChange='javascript: toggle_daily_flag( "chart.daily.showIndoorHumidity" );'/>
-&nbsp;Outdoor<input type='checkbox' id='chart.daily.showOutdoorHumidity' name='chart.daily.showOutdoorHumidity' onChange='javascript: toggle_daily_flag( "chart.daily.showOutdoorHumidity" );'/>
-
-<input type='button' onClick='javascript: deleteCookies(0);' value='Un-save settings' style='float: right;'>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; humidity Indoor<input type='checkbox' id='chart.daily.showIndoorHumidity' name='chart.daily.showIndoorHumidity'/>
+&nbsp;Outdoor<input type='checkbox' id='chart.daily.showOutdoorHumidity' name='chart.daily.showOutdoorHumidity'/>
 
 <br>
 
 <input type='button' onClick='javascript: interval(-1);' value='Previous' title='Show previous timeframe' >
-Timeframe <input type='text' id='chart.daily.interval.length' onChange='javascript: saveDateData( "daily" );' value='7' size='3'>
-<select id='chart.daily.interval.group' onChange='javascript: saveDateData( "daily" );' style='width: 65px'>
+Timeframe <input type='text' id='chart.daily.interval.length' value='7' size='3'>
+<select id='chart.daily.interval.group' style='width: 65px'>
   <option value='0' selected>days</option>
   <option value='1'>weeks</option>
   <option value='2'>months</option>
@@ -147,50 +120,117 @@ Timeframe <input type='text' id='chart.daily.interval.length' onChange='javascri
 <?php
   $show_date = date( 'Y-m-d', time() );
 ?>
-ending on <input type='date' id='chart.daily.toDate' onChange='javascript: saveDateData( "daily" );' size='10' value='<?php print( $show_date ); ?>' max='<?php print( $show_date ); ?>' step='1'/>
+ending on <input type='date' id='chart.daily.toDate' size='10' value='<?php print( $show_date ); ?>' max='<?php print( $show_date ); ?>' step='1'/>
 <input type='button' onClick='javascript: interval(1);' value='Next' title='Show next timeframe' >
 
 <div class='content'>
 <br>
+  <div id='chartdiv' style='height: 370px; width: 95%;'></div>
   <div class='thermo_chart'>
-    <img id='daily_temperature_chart' src='images/daily_temperature_placeholder.png' alt='The temperatures'>
   </div>
-
-  <input type='button' onClick='javascript: display_chart( "daily", "table" );' value='Chart it' style='float: right;'>
-  <div id='daily_temperature_table' class='status daily_temperature_table'></div>
 </div>
 
 
-<script type='text/javascript'>
-/*
-    // In JavaScript, a literal/string value of "false" is a string of non zero length and so it tests as logically true unless you look for the literal string "true"
+<script type="text/javascript">
+var chart = AmCharts.makeChart("chartdiv", {
+    "type":                 "serial"
+    ,"theme":                "light"
+    ,"marginRight":           40
+    ,"marginLeft":            40
+    ,"autoMarginOffset":      20
+    ,"mouseWheelZoomEnabled": true
+    ,"dataDateFormat":       "YYYY-MM-DD JJ:NN"
+    ,"valueAxes": [{
+        "id":             "v1"
+        ,"axisAlpha":       0
+        ,"title":          "Temperatures"
+        ,"position":       "left"
+        ,"maximum": 80
+        ,"minimum": 70
+        ,"ignoreAxisWidth": true
+    }]
+    ,"balloon": {
+         "borderThickness": 1
+        ,"shadowAlpha":     0
+    }
+    ,"graphs": [{
+        "id": "g1",
+        "balloon":{
+          "drop":              true,
+          "adjustBorderColor": false,
+          "color":            "#ffffff"
+        },
+        "bullet":           "round",
+        "bulletBorderAlpha": 1,
+        "bulletColor":      "#FFFFFF",
+        "bulletSize":        5,
+        "hideBulletsCount":  50,
+        "lineThickness":               2,
+        "title":                      "red line",
+        "useLineColorForBulletBorder": true,
+        "valueField":                 "value",
+        "balloonText":                "<span style='font-size:16px;'>[[value]]</span>"
+    }],
 
-    // Restore user preference/cookie for showing/hiding setpoints
-if( getCookie('chart.daily.setpoint') == 'true' ){
-  document.getElementById('chart.daily.setpoint').checked = true;
+    "chartScrollbar": {
+        "graph": "g1",
+        "oppositeAxis":false,
+        "offset":30,
+        "scrollbarHeight": 80,
+        "backgroundAlpha": 0,
+        "selectedBackgroundAlpha": 0.1,
+        "selectedBackgroundColor": "#888888",
+        "graphFillAlpha": 0,
+        "graphLineAlpha": 0.5,
+        "selectedGraphFillAlpha": 0,
+        "selectedGraphLineAlpha": 1,
+        "autoGridCount":true,
+        "color":"#AAAAAA"
+    },
+    "chartCursor": {
+        "pan": true,
+        "categoryBalloonDateFormat": "YYYY-MM-DD JJ:NN",
+        "cursorPosition":            "mouse",
+        "valueLineEnabled":           true,
+        "valueLineBalloonEnabled":    true,
+        "cursorAlpha":                1,
+        "cursorColor":               "#258cbb",
+        "limitToGraph":              "g1",
+        "valueLineAlpha":             0.2,
+        "valueZoomable":              true
+    },
+    "valueScrollbar":{
+      "oppositeAxis":    false,
+      "offset":          50,
+      "scrollbarHeight": 10
+    },
+    "categoryField": "date",
+    "categoryAxis": {
+        "parseDates":       false,
+        "equalSpacing":     true,
+        "title":           "Date/Time",
+        "dateFormat":      "YYYY-MM-DD JJ:NN",
+        "dashLength":       1,
+    "autoGridCount": false,
+    "gridCount": 5,
+        "minorGridEnabled": true
+    },
+    "export": {
+        "enabled": true
+    },
+    "dataProvider": rawData
+});
+
+chart.addListener("rendered", zoomChart);
+
+zoomChart();
+
+function zoomChart() {
+    chart.zoomToIndexes(chart.dataProvider.length - 40, chart.dataProvider.length - 1);
 }
 
-    // Restore user preference/cookie for showing/hiding AC operation
-if( getCookie('chart.daily.showCool') == 'true' ){
-  document.getElementById('chart.daily.showCool').checked = true;
-}
-
-    // Restore user preference/cookie for showing/hiding heater operation
-if( getCookie('chart.daily.showHeat') == 'true' ){
-  document.getElementById('chart.daily.showHeat').checked = true;
-}
-
-    // Restore user preference/cookie for showing/hiding fan operation
-if( getCookie('chart.daily.showFan') == 'true' ){
-  document.getElementById('chart.daily.showFan').checked = true;
-}
-*/
-
-//  loadDateData( 'daily' );
-
-    // Draw the graph using the applied settings
-$( document ).ready( function(){
-  display_chart( 'daily', 'chart' );
+$( document ).ready(function() {
+  display_chart();
 });
 </script>
 
