6ccbac7cfceed8b2300c3d4ef39290148553bad8
ThermoMan <thestalwart1-tstat@yahoo.com>
2020-01-27 19:08:54 -0600

    Oops forgot this in the weather API update
    Oops forgot this in the weather API update

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ define( 'DB_PORT', '3306' );
 define( 'DB_NAME', 'thermo' );
 define( 'DB_USER', 'user' );
 define( 'DB_PASS', 'password' );
-define( 'DB_PREFIX', 'thermo3__' );   // Can also use NULL or empty string
+define( 'DB_PREFIX', 'thermo__' );   // Can also use NULL or empty string
 // Prefix to attach to all table/procedure names to make unique in unknown environment.
 // Using a double underscore as the end of the prefix enables some UI magic in phpMyAdmin
 
@@ -22,11 +22,14 @@ define( 'EMAIL_USER', 'gmailuser@gmail.com' );
 define( 'EMAIL_PASS', 'gmailpass' );
 define( 'EMAIL_NAME', 'Smart Home Info' );
 
-# WEATHER_TYPE is on of: weatherunderground, noaa, weatherbug
+# WEATHER_TYPE is on of: openweathermap, noaa or NULL
+# weatherbug seems dead.
+# As of 2018/2/31 Weather Underground no longer supports the API (so no more WEATHER_TYPE of weatherunderground)
+# For more info, see: https://www.wunderground.com/weather/api/
 # WEATHER_API_LOC may be NULL (but NEVER blank ''), http://w1.weather.gov/xml/current_obs/KUZA.xml
-define( 'WEATHER_TYPE', 'weatherunderground' );
+define( 'WEATHER_TYPE', 'openweathermap' );
 define( 'WEATHER_API_KEY', '0000000000000000' );
-define( 'WEATHER_API_LOC', NULL );
+define( 'WEATHER_API_LOC', 'https://api.openweathermap.org/data/2.5' );
 
 // Variable to define site super admin
 define( 'SITE_ADMIN', 'site_admin' );
